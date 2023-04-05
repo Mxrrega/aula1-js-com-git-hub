c144672390c50e0f1128cfe498e09743ff59f3b6
Mxrrega <129889649+Mxrrega@users.noreply.github.com>
2023-04-05 11:57:17 -0300

    teste de soma

--- main.js
+++ main.js
@@ -13,3 +13,13 @@ function exibenome()
     document.getElementById( "mensagem" ).innerHTML = "Meu nome é:" + usuario;
     document.getElementById( "nome" ).value = "";
 }
+
+document.getElementById( "bnt-soma" ).addEventListener( "click" , soma );
+
+function soma ()
+{
+    let num1 = document.getElementById( "num1" ).value;
+    let num2 = document.getElementById( "num2" ).value;
+    let num3 = parseFloat(num1) + parseFloat(num2);
+    document.getElementById( "resultado1" ).innerHTML = "Result" + num3;
+}
